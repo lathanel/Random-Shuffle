03d69f90f4546cbad8da20632311382706843d35
lathanel <65995654+lathanel@users.noreply.github.com>
2021-06-18 09:47:16 +1200

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,35 @@
+from random import shuffle
+names = [
+    ["Vidit"],
+    ["Rayan"],
+    ["Mohammed"],
+    ["Siva"],
+    ["Aj"],
+    ["Malaika"],
+    ["Natan"],
+    ["Akul"],
+    ["Hafidz"],
+    ["Ayesha"],
+    ["Anshul"],
+    ["Nehmia"],
+    ["George"],
+    ["Dennon"],
+    ["Aryan"],
+    ["Isaac"],
+    ["Jack"],
+    ["Vrishank"],
+    ["Keshava"],
+    ["Natanon"],
+    ["Guransh"],
+    ["Nihall"],
+    ["Joel"],
+    ["Ishaan"],
+    ["William"],
+    ["Akul"],
+    ["Sean"],
+    ["Jim"],
+    ]
+shuffle(names)
+data = names[0]
+n = data[0]
+print("Next person to be checked by miss is",n)
